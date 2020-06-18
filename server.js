d62b1677d8d8c6974fd06d93049b5bb54fe766e5
Igor Oliveira <igoroliveira2798@gmail.com>
2020-06-18 18:00:09 -0300

    Server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const server = express();
+
+server.use(express.static("public"))
+
+const nunjucks = require("nunjucks");
+
+nunjucks.configure("./", {
+    express: server
+})
+
+server.get("/", (req, res) => {
+    return res.render("index.html")
+});
+
+server.listen(3000)
